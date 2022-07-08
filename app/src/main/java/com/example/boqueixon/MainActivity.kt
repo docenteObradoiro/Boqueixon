7632dcb6d21ffb4e8c2b95a8426676bfb35d1a86
TatianaTrians <tatiana.obradoiro@gmail.com>
2022-07-08 14:51:39 +0200

    Pico Sacro, falta multimedia

--- app/src/main/java/com/example/boqueixon/MainActivity.kt
+++ app/src/main/java/com/example/boqueixon/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.boqueixon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var picoSacro=findViewById<Button>(R.id.buttonPicoSacro)
+        picoSacro.setOnClickListener {
+            val acceder= Intent(this, PicoSacro::class.java)
+            startActivity(acceder)
+        }
     }
 }
