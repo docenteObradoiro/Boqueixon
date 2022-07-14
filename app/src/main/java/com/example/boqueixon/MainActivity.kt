7eee242b9df88103a8f40aee7a87984aa5b7653e
TatianaTrians <tatiana.obradoiro@gmail.com>
2022-07-14 10:44:07 +0200

    Creación páxina principal e modificacións de Pico Sacro

--- app/src/main/java/com/example/boqueixon/MainActivity.kt
+++ app/src/main/java/com/example/boqueixon/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.boqueixon
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.boqueixon.Alojamiento.Alojamiento
 import androidx.appcompat.app.AlertDialog
 import com.example.boqueixon.gastronomia.Gastronomia
@@ -43,6 +46,40 @@ class MainActivity : AppCompatActivity() {
             startActivity(acceder)
         }
 
+        var folletoCastellano=findViewById<ImageView>(R.id.banderaEspana)
+        folletoCastellano.setOnClickListener {
+            startActivity(Intent(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.boqueixon.com/contido/subidas/2018/08/folleto-ES-co%C3%B1ece-boqueixon-online.pdf"))))
+        }
+
+        var folletoGallego=findViewById<ImageView>(R.id.banderaGalicia)
+        folletoGallego.setOnClickListener {
+            startActivity(Intent(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.boqueixon.com/contido/subidas/2018/08/folleto-GL-co%C3%B1ece-boqueixon.pdf"))))
+        }
+
+        var folletoIngles=findViewById<ImageView>(R.id.banderaUk)
+        folletoIngles.setOnClickListener {
+            startActivity(Intent(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.boqueixon.com/contido/subidas/2018/08/folleto-EN-co%C3%B1ece-boqueixon-online.pdf"))))
+        }
+
+        var cookies=findViewById<TextView>(R.id.cookies)
+        cookies.setOnClickListener {
+            startActivity(Intent(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.boqueixon.gal/politica-de-cookies/"))))
+        }
+
+        var privacidade=findViewById<TextView>(R.id.privacidad)
+        privacidade.setOnClickListener {
+            startActivity(Intent(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.boqueixon.gal/lopd/"))))
+        }
+
+        var responsabilidad=findViewById<TextView>(R.id.responsabilidade)
+        responsabilidad.setOnClickListener {
+            startActivity(Intent(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.boqueixon.gal/descargo-de-responsabilidad/"))))
+        }
+
+        var concello=findViewById<TextView>(R.id.webBoqueixon)
+        concello.setOnClickListener {
+            startActivity(Intent(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.boqueixon.gal/"))))
+        }
         }
 
     override fun onBackPressed() {
